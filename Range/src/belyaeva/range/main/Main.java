9eeb658de56828bf2a3028bfe2e2201b89a655a5
belyaeval <belen@yandex.ru>
2019-07-12 03:16:46 +0700

    Задача Range*

--- Range/src/belyaeva/range/main/Main.java
+++ Range/src/belyaeva/range/main/Main.java
@@ -4,16 +4,46 @@ import belyaeva.range.task.Range;
 
 public class Main {
     public static void main(String[] args) {
-        Range range = new Range(7.2, 15);
+        Range range1 = new Range(5, 12);
+        Range range2 = new Range(3, 17.6);
         double number = 9.4;
 
-        System.out.println(range.getLength());
-        System.out.println(range.isInside(number));
+        System.out.println("Length = " + range1.getLength());
+        System.out.println("IsInside = " + range1.isInside(number));
+        System.out.printf("Distance = %.2f%n", range1.getDistance(range2));
+        System.out.println();
 
-        range.setFrom(6.5);
-        range.setTo(10.8);
+        Range iRange = range1.getIntersection(range2);
 
-        System.out.println(range.getFrom());
-        System.out.println(range.getTo());
+        if (iRange != null) {
+            System.out.println("IntersectionFrom = " + iRange.getFrom());
+            System.out.println("IntersectionTo = " + iRange.getTo());
+        } else {
+            System.out.println("Intersection = " + null);
+        }
+        System.out.println();
+
+        range1.setFrom(5);
+        range1.setTo(12);
+
+        Range uRange = range1.getUnion(range2);
+
+        System.out.println("UnionFrom = " + uRange.getFrom());
+        System.out.println("UnionTo = " + uRange.getTo());
+        System.out.println();
+
+        range1.setFrom(5);
+        range1.setTo(12);
+
+        Range[] dRange = range1.getDifference(range2);
+
+        for (Range range : dRange) {
+            if (range != null) {
+                System.out.println("DifferenceFrom = " + range.getFrom());
+                System.out.println("DifferenceTo = " + range.getTo());
+            } else {
+                System.out.println("Difference = " + null);
+            }
+        }
     }
 }
--- Range/src/belyaeva/range/task/Range.java
+++ Range/src/belyaeva/range/task/Range.java
@@ -32,4 +32,55 @@ public class Range {
     public boolean isInside(double number) {
         return number >= from && number <= to;
     }
+
+    public double getDistance(Range r) {
+        return Math.sqrt(Math.pow((from - r.from), 2) + Math.pow((to - r.to), 2));
+    }
+
+    public Range getIntersection(Range r) {
+        if (r.from > to || r.to < from) {
+            return null;
+        }
+        if (isInside(r.from)) {
+            from = r.from;
+        }
+        if (isInside(r.to)) {
+            to = r.to;
+        }
+        return new Range(from, to);
+    }
+
+    public Range getUnion(Range r) {
+        if (!isInside(r.from) && r.from < from) {
+            from = r.from;
+        }
+        if (!isInside(r.to) && r.to > to) {
+            to = r.to;
+        }
+        return new Range(from, to);
+    }
+
+    public Range[] getDifference(Range r) {
+        double checkFrom = from;
+        double checkTo = to;
+        Range forCheck = new Range(checkFrom, checkTo);
+        Range[] range = new Range[2];
+
+        if (getIntersection(r) == null) {
+            range[0] = forCheck;
+        } else if (from > forCheck.from && to < forCheck.to) {
+            double from1 = to + 0.1;
+            double to1 = checkTo;
+            checkTo = from - 0.1;
+            range[0] = new Range(checkFrom, checkTo);
+            range[1] = new Range(from1, to1);
+        } else if (from > forCheck.from) {
+            checkTo = from - 0.1;
+            range[0] = new Range(checkFrom, checkTo);
+        } else if (to < forCheck.to) {
+            checkFrom = to + 0.1;
+            range[0] = new Range(checkFrom, checkTo);
+        }
+        return range;
+    }
 }
